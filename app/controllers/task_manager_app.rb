10d150526bac9e65d669df98b48c697646d66b42
samuelssnider <sam_redins@yahoo.com>
2017-08-11 12:03:28 -0600

    done

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -8,13 +8,18 @@ class TaskManagerApp < Sinatra::Base
   end
 
 	get '/tasks' do
-		@tasks = ["task1", "task2", "task3"]
-    erb :index
+		@tasks = Task.all
+  	erb :index
 	end
 
 	get '/tasks/new' do
     erb :new
   end
+
+	get '/tasks/:id' do
+  	@task = Task.find(params[:id])
+  	erb :show
+	end
 
 	post '/tasks' do
 		task = Task.new(params[:task])
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,33 @@
+require 'sqlite3'
 class Task
+	attr_reader :title, :description, :id
   def initialize(task_params)
+		@database = SQLite3::Database.new('db/task_manager_development.db')
+		@database.results_as_hash = true
     @description = task_params["description"]
     @title       = task_params["title"]
+		@id = task_params["id"] if task_params["id"]
   end
+
+	def save
+  	@database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
+	end
+
+	def self.all
+	  tasks = database.execute("SELECT * FROM tasks")
+	  tasks.map do |task|
+	    Task.new(task)
+	  end
+	end
+	def self.find(id)
+	  task = database.execute("SELECT * FROM tasks WHERE id = ?", id.to_i).first
+	  Task.new(task)
+	end
+
+	def self.database
+		database = SQLite3::Database.new('db/task_manager_development.db')
+	  database.results_as_hash = true
+		database
+	end
+
 end
